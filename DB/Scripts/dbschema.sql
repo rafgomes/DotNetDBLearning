3b5d450e6c928ea793fa6901dfd4f8ca3f7206a0
milocaetano <camilo.s.caetano@gmail.com>
2022-11-18 22:11:25 -0300

    atualiza script de banco

--- DB/Scripts/dbschema.sql
+++ DB/Scripts/dbschema.sql
@@ -1,17 +1,22 @@
+
 USE [BancoTeste]
 GO
 
-/****** Object:  Table [dbo].[Lista]    Script Date: 17/11/2022 00:14:53 ******/
+/****** Object:  Table [dbo].[Lista]    Script Date: 18/11/2022 22:09:44 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
+
+DROP TABLE Lista
+
+
 CREATE TABLE [dbo].[Lista](
-	[ID] [int] NOT NULL,
 	[Nome] [nchar](200) NOT NULL,
-	[Telefone] [nchar](30) NOT NULL
+	[Telefone] [nchar](30) NOT NULL,
+	[ListaId] [int] IDENTITY(1,1) NOT NULL
 ) ON [PRIMARY]
 GO
 
